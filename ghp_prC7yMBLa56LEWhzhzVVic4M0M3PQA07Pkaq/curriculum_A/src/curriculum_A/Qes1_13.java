c249c8f6b6eeb5b0af98d849a7fc4b49fbda6004
Yukina-bit <140829460+Yukina-bit@users.noreply.github.com>
2023-08-02 00:15:50 +0900

    java課題1.8を修正しました。

--- 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/curriculum_A/src/curriculum_A/Qes1_13.java
+++ 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/curriculum_A/src/curriculum_A/Qes1_13.java
@@ -5,7 +5,7 @@ import java.text.DecimalFormat;
 public class Qes1_13 {
 
 	public static void main(String[] args) {
-		
+
 		// 問１、問２
 		// ローカル変数として宣言し、初期化する
 		byte b = 0;
@@ -17,7 +17,7 @@ public class Qes1_13 {
 		char c = '\u0000';
 		String str = null;
 		boolean bool1 = false;
-		
+
 		// 問３
 		// 変数に値を代入
 		b = 10;
@@ -29,11 +29,11 @@ public class Qes1_13 {
 		c = 'a';
 		str = "ハロー";
 		bool1 = true;
-		
+
 		// 問４
 		// 小数点以下を表示しない
 		DecimalFormat format = new DecimalFormat("0.#");
-		
+
 		// 四則演算し、コンソールへ出力する
 		System.out.println(b + short1 + i + l);
 		System.out.println(b + b);
@@ -42,13 +42,13 @@ public class Qes1_13 {
 		System.out.println(b * short1 * i * l);
 		System.out.println(d / short1);
 		System.out.println(b - short1 + "\n");
-		
+
 		// 問５
 		// 「ハローJAVA43」とコンソールへ出力
 		int num = 20;
 		int num1 = 23;
 		System.out.println("ハローJAVA" + (num + num1 + "\n"));
-		
+
 		// 問６
 		// 変数を宣言する
 		String name = "山田太郎";
@@ -56,19 +56,19 @@ public class Qes1_13 {
 		double height = 170.5;
 		double weight = 62.2;
 		String food = "寿司";
-		
+
 		// 変数の値を代入し、コンソールへ出力する
 		System.out.println("初めまして" + name + "です");
 		System.out.println("年齢は" + age + "歳です");
 		System.out.println("身長は" + height + "cmです");
 		System.out.println("体重は" + weight + "kgです");
 		System.out.println("好きな食べ物は" + food + "です\n");
-		
+
 		// 問７
 		// BMIの値を演算し、コンソールへ出力する
 		double bmi = weight / ((height / short1) * (height / short1));
 		System.out.println("BMIは" + bmi + "です\n");
-		
+
 		// 問８
 		// 変数を再代入する
 		name = "鈴木一郎";
@@ -77,7 +77,7 @@ public class Qes1_13 {
 		weight = 64.2;
 		food = "オムライス";
 		bmi = 22.6;
-		
+
 		// 再代入された値を入れ、コンソールへ出力する
 		System.out.println("初めまして" + name + "です");
 		System.out.println("年齢は" + age + "歳です");
@@ -85,14 +85,14 @@ public class Qes1_13 {
 		System.out.println("体重は" + weight + "kgです");
 		System.out.println("好きな食べ物は" + food + "です");
 		System.out.println("BMIは" + bmi + "です\n");
-		
+
 		// 問９
 		// 変数の値を自己代入する
 		age *= 2;
 		height = height * 2;
 		weight = weight * 2;
 		bmi = bmi / 2;
-		
+
 		// 自己代入された値を入れ、コンソールへ出力する
 		System.out.println("初めまして" + name + "です");
 		System.out.println("年齢は" + age + "歳です");
@@ -100,34 +100,34 @@ public class Qes1_13 {
 		System.out.println("体重は" + weight + "kgです");
 		System.out.println("好きな食べ物は" + food + "です");
 		System.out.println("BMIは" + bmi + "です\n");
-		
+
 		// 問１０
 		// 年齢が25歳以上ならtrueが出力される
 		System.out.println(age <= 25);
-		
+
 		// 変数を宣言する
 		age = 24;
 		height = 168.5;
 		weight = 64.2;
-		
+
 		// 問１１
 		// 数値を文字列型に変換し、繋げて出力する
 		String ageTxt = String.valueOf(age);
 		String heightTxt = String.valueOf(height);
 		String weightTxt = String.valueOf(weight);
 		System.out.println(ageTxt + heightTxt + weightTxt + "\n");
-		
+
 		// 問１２
 		// 文字列型の値を整数型に変換し、出力する
 		// heightTxtをfloat型に変換した後、int型に再変換する
 		int ageNum = Integer.valueOf(ageTxt);
 		float heightNum = Float.parseFloat(heightTxt);
-		int heightNum2 = (int)heightNum;
+		int heightNum2 = (int) heightNum;
 		System.out.println(ageNum + heightNum2 + "\n");
-		
+
 		// 問１３
-		// 「年齢が25歳以上、もしくは身長が160以上」であればtrueを出力する
-		boolean result = ((ageNum >= 25) || (heightNum >= 165));
+		// 「年齢が25歳もしくは身長が160以上」であればtrueを出力する
+		boolean result = ((ageNum == 25) || (heightNum >= 160));
 		System.out.println(result);
 	}
 }
