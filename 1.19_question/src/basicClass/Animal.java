16fb494fe9edf9103434b2178a8d3605a902b670
Yukina-bit <140829460+Yukina-bit@users.noreply.github.com>
2023-08-16 22:35:12 +0900

    java課題1.19が完了したので提出いたします。

---
+++ 1.19_question/src/basicClass/Animal.java
@@ -0,0 +1,32 @@
+package basicClass;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+
+		// Q1 
+		// Dogクラスを呼び出す
+		Dog dogClass = new Dog();
+		// Dogクラス 変数godを出力する
+		System.out.println(dogClass.dog + "\n");
+
+		// Q2
+		// Dogクラス 変数dogNumを出力する
+		System.out.println(dogClass.dogNum + "\n");
+
+		// Q3
+		// 日付（今回は現在の日時）を取得する
+		Date exDate = new Date();
+		// 「yyyy-MM-dd H:m:s」形式に指定する
+		SimpleDateFormat exDateFormat = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+		// フォーマットを指定し出力する
+		System.out.println(exDateFormat.format(exDate));
+
+	}
+}
