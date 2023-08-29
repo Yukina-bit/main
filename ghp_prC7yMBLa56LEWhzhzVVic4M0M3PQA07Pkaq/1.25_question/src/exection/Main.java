43c829f7c3da304a99f30ae27954ecdc5e55df83
Yukina-bit <140829460+Yukina-bit@users.noreply.github.com>
2023-08-30 06:47:57 +0900

    1.25課題を修正しました。

--- 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/1.25_question/src/exection/Main.java
+++ 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/1.25_question/src/exection/Main.java
@@ -1,57 +1,17 @@
 package exection;
 
 import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
 
 import process.Sub;
-import process.Sub.StrSub;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		// クラスをインスタンス化する
 		Sub sub = new Sub();
-		StrSub strsub = sub.new StrSub();
-
-		// ランダムな値を作成する
-		Random rand = new Random();
-
-		// setterメソッドに変数を渡す
-		sub.setHp(rand.nextInt(999));
-		sub.setMp(rand.nextInt(999));
-		sub.setAttack(rand.nextInt(999));
-		sub.setSpeed(rand.nextInt(999));
-		sub.setDefence(rand.nextInt(999));
-
-		// ユーザー名を入力してもらう
-		Scanner scanner = new Scanner(System.in);
-		String name = scanner.nextLine();
-
-		// 名前が未入力の時に出力する
-		if (name.length() == 0) {
-			System.out.println("名前を入力してください");
-			return;
-		}
 
 		if (Objects.nonNull(sub)) {
-			if (Objects.nonNull(strsub)) {
-				strsub.greeting1();
-				System.out.print(name);
-				strsub.greeting2();
-				System.out.println("HP：" + sub.getHp());
-				System.out.println("MP：" + sub.getMp());
-				System.out.println("攻撃力：" + sub.getAttack());
-				System.out.println("素早さ：" + sub.getSpeed());
-				System.out.println("防御力：" + sub.getDefence());
-				// 改行する
-				System.out.println();
-				strsub.endingStr();
-
-			} else {
-				System.out.println("strsubがnullです");
-			}
+			sub.main();
 		} else {
 			System.out.println("subがnullです");
 		}
--- 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/1.25_question/src/process/Sub.java
+++ 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/1.25_question/src/process/Sub.java
@@ -1,5 +1,7 @@
 package process;
 
+import java.util.Random;
+import java.util.Scanner;
 /*
  * 名前を入力したら下記がコンソールに出力されるように作ってください
     条件：数値は毎回変わるように作ってください
@@ -21,28 +23,60 @@ package process;
     さあ冒険に出かけよう！
  * 
  */
+
 public class Sub {
-	
+	Random rand = new Random();
+	AbilitySub sub = new AbilitySub(rand.nextInt(999), rand.nextInt(999),
+			rand.nextInt(999), rand.nextInt(999), rand.nextInt(999));
+
+	public void main() {
+		// ユーザー名を入力してもらう
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+
+		// 未入力の場合、エラー文を出す
+		if (name.length() == 0) {
+			System.out.println("名前を入力してください");
+		} else {
+			// 上記以外の場合、出力する
+			System.out.println("こんにちは「 " + name
+					+ " 」さん\nステータス");
+			System.out.println(sub.toString() + "\n");
+			System.out.println("さあ冒険に出かけよう！");
+		}
+		// scaanerを閉じる
+		scanner.close();
+	}
+}
+
+// クラスを作成する
+class Ability {
 	// フィールドを宣言する
-	public int hp;
-	public int mp;
-	public int attack;
-	public int speed;
-	public int defence;
+	int hp;
+	int mp;
+	int attack;
+
+	public Ability(int hp, int mp, int attack) {
+		this.hp = hp;
+		this.mp = mp;
+		this.attack = attack;
+	}
+
+	// 出力用のメソッドを作成する
+	public String toString() {
+		return "HP：" + this.hp + "\n" + "MP：" + this.mp + "\n" +
+				"攻撃力：" + this.attack + "\n";
+	}
+
+}
+
+// サブクラスを作成する
+class AbilitySub extends Ability {
+	// フィールドを作成する
+	int speed;
+	int defence;
 
 	// getterを作成する
-	public int getHp() {
-		return hp;
-	}
-
-	public int getMp() {
-		return mp;
-	}
-
-	public int getAttack() {
-		return attack;
-	}
-
 	public int getSpeed() {
 		return speed;
 	}
@@ -52,18 +86,6 @@ public class Sub {
 	}
 
 	// setterを作成する
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-
-	public void setMp(int mp) {
-		this.mp = mp;
-	}
-
-	public void setAttack(int attack) {
-		this.attack = attack;
-	}
-
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
@@ -71,21 +93,17 @@ public class Sub {
 	public void setDefence(int defence) {
 		this.defence = defence;
 	}
-	
-	// サブクラスを作成する
-	public class StrSub extends Sub {
-		// メソッドを作成する
-		public void greeting1() {
-			System.out.print("こんにちは「 ");
-		}
 
-		public void greeting2() {
-			System.out.println(" 」さん\nステータス");
-		}
-
-		public void endingStr() {
-			System.out.println("さあ冒険に出かけよう！");
-		}
-
+	public AbilitySub(int hp, int mp, int attack, int speed, int defence) {
+		super(hp, mp, attack);
+		this.speed = speed;
+		this.defence = defence;
 	}
+
+	// メソッドを作成する
+	public String toString() {
+		return super.toString() + "素早さ：" + getSpeed() + "\n" +
+				"攻撃力：" + getDefence();
+	};
+
 }
